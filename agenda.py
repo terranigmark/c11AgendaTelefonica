a2326fb3c6651395cdeba4d2239be7faa5e1b6eb
terranigmark <linnk99@gmail.com>
2024-06-24 18:03:48 -0700

    Menu creado para la agenda

--- agenda.py
+++ agenda.py
@@ -1,1 +1,17 @@
-git
+
+
+def mostrar_menu():
+    print("\n===Agenda Telefonica===")
+    print("1. Agregar contacto")
+    print("2. Buscar contacto")
+    print("3. Mostrar todos los contactos")
+    print("4. Salir de la aplicacion")
+    opcion = int(input("\nIngresa una opcion del menu: "))
+    return opcion
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
