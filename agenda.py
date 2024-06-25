8baed6c52facf10f03c6a0011b027c01c041ce96
terranigmark <linnk99@gmail.com>
2024-06-24 18:14:50 -0700

    Se agregaron acentos y caracteres en espanol

--- agenda.py
+++ agenda.py
@@ -1,13 +1,14 @@
 
 
 def mostrar_menu():
-    print("\n===Agenda Telefonica===")
-    print("1. Agregar contacto")
+    print("\n===Agenda Telefónica===")
+    print("1. Añadir contacto")
     print("2. Buscar contacto")
     print("3. Mostrar todos los contactos")
-    print("4. Salir de la aplicacion")
-    opcion = int(input("\nIngresa una opcion del menu: "))
+    print("4. Salir de la aplicación")
+    opcion = int(input("\nIngresa una opción del menu: "))
     return opcion
+
 
 def main():
     pass
