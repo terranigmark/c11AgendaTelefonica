67fc38f6c37e3055f53bff6cc04128a64e413257
terranigmark <linnk99@gmail.com>
2024-06-24 18:33:41 -0700

    Funcion de buscar contacto agregado

--- agenda.py
+++ agenda.py
@@ -3,10 +3,10 @@
 # Menu de opciones para la aplicacion
 ## El usuario debe ingresar un numero de las opciones valida
 def mostrar_menu():
-    print("\n===Agenda Telefónica===")
-    print("1. Añadir contacto")
+    print("\n######Agenda Telefónica####")
+    print("1. Agregar contacto")
     print("2. Buscar contacto")
-    print("3. Mostrar todos los contactos")
+    print("3. Mostrar todos los personas")
     print("4. Salir de la aplicación")
     opcion = int(input("\nIngresa una opción del menu: "))
     return opcion
@@ -15,7 +15,15 @@ def mostrar_menu():
 def agregar_contacto(agenda, nombre, telefono):
     # Diccionario
     agenda[nombre] = telefono
-    return f"\nContacto agregado exitosamente"
+    return f"\nContacto agregado con exito"
+
+
+def buscar_contacto(agenda, nombre):
+    # TODO: Que el return al encontrar contacto muestre su nombre
+    if nombre in agenda:
+        return f"\nTelefono: {agenda[nombre]}"
+
+    return "\nContacto no encontrado"
 
 
 # Aqui va la logica del programa
